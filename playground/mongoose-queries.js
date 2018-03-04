8ae80d274cf68e71d792a0fa832def7ce5ad50cc
Mario <stepenik@gmail.com>
2018-03-04 21:22:46 +0100

    Add queries playgroud

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,39 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// var id = '5a9c3d07b3f12dac03a57cf3000';
+//
+// if(!ObjectID.isValid(id)){
+//   console.log('ID not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if(!todo){
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));
+
+User.findById('5a99cb99ff29dee30a248145').then((user) =>{
+  if(!user){
+    return console.log('Unable to find user');
+  }
+  
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) =>{
+  console.log(e);
+});
