cab8ba9a80b948648725ca2037988d0da62b22e6
Mario <stepenik@gmail.com>
2018-03-04 23:05:42 +0100

    Setup project

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://crosite:crosite123@@ds255958.mlab.com:55958/mongo-udemy')
+mongoose.connect('mongodb://crosite:crosite123@ds255958.mlab.com:55958/mongo-udemy')
 
 module.exports = {mongoose};
